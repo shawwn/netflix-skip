877dfccb1f5e6808cc28adb4935ea414c6858566
Shawn Presser <shawnpresser@gmail.com>
2021-09-15 16:02:48 -0500

    Fix intro skip + simplify logic

--- js/watch.js
+++ js/watch.js
@@ -1,7 +1,5 @@
 let setting,
-  skipList = [],
-  setChange = false,
-  pathName = "";
+  skipList = [];
 
 const makeSkipList = (setting) => {
   let intro = false;
@@ -32,60 +30,30 @@ chrome.storage.sync.get("netflixAutoSkip_setting_v133", function (items) {
 chrome.storage.onChanged.addListener(function (changes, namespace) {
   setting = changes.netflixAutoSkip_setting_v133.newValue;
   skipList = makeSkipList(setting);
-  setChange = true;
 });
 
 /**************************************************************************************************** */
 
-const target = document.getElementById("appMountPoint");
+const autoClick = (btn) => {
+  if (btn) {
+    console.log('autoClick');
+    console.log(btn);
+    btn.disabled = false;
+    btn.click();
+  }
+}
 
-const observer = new MutationObserver((mutations) => {
-  mutations.forEach((mutation) => {
-    if (mutation.nextSibling && mutation.addedNodes.length) {
-      Array.from(mutation.addedNodes).filter((node) => {
-        if (node.classList && node.classList.contains("interrupter-action")) {
-          node.firstChild.click();
-        }
-
-        if (
-          skipList.intro &&
-          node.classList &&
-          node.classList.contains("skip-credits")
-        ) {
-          setTimeout(() => {
-            node.querySelector("a").click();
-          }, 200);
-        }
-
-        if (
-          skipList.next &&
-          node.className === "main-hitzone-element-container" &&
-          document.querySelectorAll("button").length === 12
-        ) {
-          setTimeout(() => {
-            document.querySelector(".button-nfplayerNextEpisode").click();
-          }, 200);
-        }
-      });
-    }
-  });
-});
-
-const config = { childList: true, subtree: true };
+const autoSkipAll = () => {
+  if (skipList.intro) {
+    autoClick(document.querySelector('.watch-video--skip-content-button'));
+  }
+  if (skipList.next) {
+    autoClick(document.querySelector('button[data-uia=next-episode-seamless-button]'));
+  }
+}
 
 const watchInterval = setInterval(() => {
-  const newPathName = window.location.pathname;
-  if (newPathName !== pathName) {
-    if (newPathName.indexOf("watch") !== -1) {
-      observer.disconnect();
-      observer.observe(target, config);
-    } else {
-      observer.disconnect();
-    }
-  } else if (setChange) {
-    observer.disconnect();
-    observer.observe(target, config);
-    setChange = false;
+  if (window.location.pathname.indexOf('watch') !== -1) {
+    autoSkipAll();
   }
-  pathName = newPathName;
-}, 3000);
+}, 1000);
